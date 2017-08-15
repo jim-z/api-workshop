2ab0bea0478ad4ddfa749d2e6198ce3e1fc4ee2a
jim-z <jim-z@users.noreply.github.com>
2017-08-15 12:55:40 -0500

    Update app.js

--- 00-workspace/app.js
+++ 00-workspace/app.js
@@ -23,3 +23,19 @@ var map = new ol.Map({
     zoom: 5
   })
 });
+
+//Change user key if you reuse this!
+
+var app= {
+  mapzenKey: 'mapzen-CpAANqF',
+  activeSearch: 'from',
+  
+  typeAhead: function(e){
+    var el=e.target;
+    var val=el.value;
+    console.log(val);
+  }
+}
+
+$('#search-from-input').on('keyup',{input:'from'},
+app.typeAhead)
